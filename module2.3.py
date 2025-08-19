8d545116a69a6ce703e4219029c26215d61f3fe7
aungthuyahan <aungthu.ya659@gmail.com>
2025-08-19 10:56:03 +0630

    3rd Commit

---
+++ module2.3.py
@@ -0,0 +1,8 @@
+rec_length = int(input("Enter length of the rectangle: "))
+rec_width = int(input("Enter width of the rectangle: "))
+
+rec_perimeter = (rec_length + rec_width) * 2
+print(rec_perimeter)
+
+rec_area = ( rec_length * rec_width)
+print(rec_area)
